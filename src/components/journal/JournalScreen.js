8a407d97a8e031b3e628c916bde79c2e93a5a132
Nicolás Trujillo <tnicolas@uninorte.edu.co>
2021-10-15 17:09:28 -0500

    NoteAppBar

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { NoteScreen } from '../../notes/NoteScreen'
+import { NothingSelected } from './NothingSelected'
 import { Sidebar } from './Sidebar'
 
 export const JournalScreen= () => {
@@ -8,7 +10,8 @@ export const JournalScreen= () => {
             <Sidebar />
             
            <main>
-               <NothingSelected />
+               {/* <NothingSelected/> */}
+               <NoteScreen />
            </main>
         </div>
     )
---
+++ src/notes/NoteScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NotesAppBar } from './NotesAppBar'
+
+export const NoteScreen = () => {
+    return (
+        <div className="notes__main-content">
+            <NotesAppBar />
+
+            <div className="notes__content">
+                
+            </div>
+        </div>
+    )
+}
