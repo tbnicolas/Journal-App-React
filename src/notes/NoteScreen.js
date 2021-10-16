3c6e32889f4b6247e54970d2dc10742c5a3c8872
Nicolás Trujillo <tnicolas@uninorte.edu.co>
2021-10-15 20:40:40 -0500

    Terminando la mayoria del diseño

--- src/notes/NoteScreen.js
+++ src/notes/NoteScreen.js
@@ -7,7 +7,25 @@ export const NoteScreen = () => {
             <NotesAppBar />
 
             <div className="notes__content">
-                
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                />
+                <textarea 
+                    name=""
+                    id=""
+                    className="notes__textarea"
+                    placeholder="What happened today"
+                >
+
+                </textarea>
+                <div className="notes__images">
+                    <img 
+                        src="http://cdn.eso.org/images/screen/millour-01-cc.jpg"
+                        alt="Landscape" 
+                    />
+                </div>
             </div>
         </div>
     )
