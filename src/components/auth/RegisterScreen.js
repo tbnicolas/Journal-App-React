2a020298c7feafbcd73add1062b23096263cc688
Nicolás Trujillo <tnicolas@uninorte.edu.co>
2021-10-21 19:24:51 -0500

    Formulario de registro de usuarios

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
 
 export const RegisterScreen = () => {
+
+     
+    const initialForm  = {
+        name:'Nicolas',
+        email:'trujillo@example.com',
+        password: 12345,
+        password2: 12345,
+    }
+
+    
+    
+    const [ formValues, handleInputChange ] = useForm(initialForm);
+
+    const { name, email, password, password2 } = formValues;
+
+    const handleRegister = ( e ) => {
+        e.preventDefault();
+        console.log( name, email, password, password2 );
+    }
+
+    const isFormValid = () => {}
+
     return (
         <div>
             <h3 className="auth__title">Register</h3>
-            <form>
+            <form onSubmit={ handleRegister }>
 
                 <input 
                     type="text"
@@ -13,6 +36,8 @@ export const RegisterScreen = () => {
                     name="name"
                     className="auth__input"
                     autoComplete="off"
+                    value={name}
+                    onChange={handleInputChange}
                 />
 
                 <input 
@@ -21,6 +46,8 @@ export const RegisterScreen = () => {
                     name="email"
                     className="auth__input"
                     autoComplete="off"
+                    value={email}
+                    onChange={handleInputChange}
                 />
 
                  <input 
@@ -28,12 +55,16 @@ export const RegisterScreen = () => {
                     placeholder="Password"
                     name="password"
                     className="auth__input"
+                    value={password}
+                    onChange={handleInputChange}
                 />
                 <input 
                     type="password"
                     placeholder="Confirm password"
                     name="password2"
                     className="auth__input"
+                    value={password2}
+                    onChange={handleInputChange}
                 />
                 <button
                     type="submit"
