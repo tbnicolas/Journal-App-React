cba71cb26741b28e5eb819079b323b8f55876a56
Nicolás Trujillo <tnicolas@uninorte.edu.co>
2021-10-21 19:52:43 -0500

    Manejo de errores del formulario

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import validator from 'validator';
 import { useForm } from '../../hooks/useForm';
+
+
 
 export const RegisterScreen = () => {
 
@@ -21,14 +24,40 @@ export const RegisterScreen = () => {
     const handleRegister = ( e ) => {
         e.preventDefault();
         console.log( name, email, password, password2 );
+
+        if ( isFormValid() ) {
+            console.log('Formulario correcto');
+        }
     }
 
-    const isFormValid = () => {}
+    const isFormValid = () => {
+
+        if ( name.trim().length === 0 ) {
+            console.log('Name is required');
+            return false;
+        } else if(! validator.isEmail(email) ) {
+            console.log('Email is not valid');
+            return false;
+        } else if   ( password !== password2 || password.length < 5 ) {
+            console.log('Password should be at least 5 characters and match each other');
+            return false;
+        } 
+
+        return true;
+        
+
+
+    }
 
     return (
         <div>
             <h3 className="auth__title">Register</h3>
             <form onSubmit={ handleRegister }>
+                
+                <div className="auth__alert-error">
+                    Hola Mundo
+                </div>
+
 
                 <input 
                     type="text"
