0003dc3ea4cf1975ca572f23abf617555d132d96
Nicolás Trujillo <tnicolas@uninorte.edu.co>
2021-10-26 17:46:38 -0500

    mostrar notas en el menu lateral

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,28 +1,39 @@
 import React from 'react'
+import moment from 'moment';
 
-export const JournalEntry = () => {
+export const JournalEntry = ({id,title,date,body,url}) => {
+    
+    const noteDate = moment(date);
+
     return (
         <div className="journal__entry pointer">
-            <div 
-                className="journal__entry-picture"
-                style={{
-                    backgroundSize: 'cover',
-                    backgroundImage: 'url(https://i.pinimg.com/originals/5f/c0/aa/5fc0aade33270bfc2ac6f8c5cb67e21f.jpg)'
-                }}
-            ></div>
+
+
+            {
+                url &&
+                <div 
+                    className="journal__entry-picture"
+                    style={{
+                        backgroundSize: 'cover',
+                        backgroundImage: `url(${ url })`
+                    }}
+                ></div>
+
+            }
+            
 
             <div className="journal__entry-body">
                 <p className="journal__entry-title">
-                    Un nuevo dia
+                    { title }
                 </p>
                 <p className="journal__entry-content">
-                 gnam autem ab praesentium excepturi ipsam possimus! Officia aut quo cum debitis similique rerum voluptas.
+                 { body }
                 </p>
             </div>
             
             <div className="journal__entry-date-box">
-                <span>Monday</span>
-                <h4>28</h4>
+                <span>{ noteDate.format('dddd') }</span>
+                <h4>{ noteDate.format('Do') }</h4>
             </div>
         </div>
     )
