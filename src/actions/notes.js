b5280bccecc56b6c2a68f7d63ffc2c45dc65533c
Nicolás Trujillo <tnicolas@uninorte.edu.co>
2021-10-27 17:42:05 -0500

    Subir imagen a Cloudinary

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -3,6 +3,7 @@ import { db } from "../firebase/firebase-config";
 import { collection, addDoc,updateDoc,doc } from "firebase/firestore";
 import { types } from "../types/types";
 import { loadNotes } from "../helpers/loadNotes";
+import { fileUpload } from '../helpers/fileUpload';
 
 export const startNewNote = () => {
 
@@ -88,5 +89,17 @@ export const  refreshNote = ( id, note ) => {
                 ...note
             }
         }
+    }
+}
+
+
+export const startUploading = ( file ) => {
+    
+    return async( dispatch,getState )=> {
+        const { active:activeNote } = getState().notes;
+
+        const fileUrl = await fileUpload( file );
+        console.log(fileUrl);
+
     }
 }
